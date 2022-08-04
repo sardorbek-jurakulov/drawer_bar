e52ce839eb959b54424d8f7a6edabadd850b6a49
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-08-04 22:12:55 +0500

    WIP

--- lib/user_chats.dart
+++ lib/user_chats.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'dart:math' as math;
 
 class UserChatPage extends StatefulWidget {
   const UserChatPage({Key? key}) : super(key: key);
@@ -19,33 +21,86 @@ class _UserChatPageState extends State<UserChatPage> {
         drawer: Drawer(
           child: ListView(
             children: [
-              DrawerHeader(
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: const [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundImage:
-                              AssetImage("assets/images/first_avatar.jpeg"),
-                        ),
-                        Icon(Icons.sunny),
-                      ],
+              SizedBox(
+                height: 197,
+                child: DrawerHeader(
+                  margin: EdgeInsets.zero,
+                  padding: EdgeInsets.zero,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                      top: 25,
+                      right: 15,
+                      bottom: 15,
                     ),
-                    Row(
+                    child: Column(
                       children: [
-                        Column(
-                          children: const [
-                            Text("Sardorbek Jo'raqulov"),
-                            Text("+998 946110464"),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const CircleAvatar(
+                              radius: 45,
+                              backgroundImage:
+                                  AssetImage("assets/images/first_avatar.jpeg"),
+                            ),
+                            // Icon(Icons.sunny, color: Colors.white),
+                            Transform(
+                              alignment: Alignment.center,
+                              transform: Matrix4.rotationY(math.pi),
+                              child: const Icon(CupertinoIcons.moon_stars_fill,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Sardorbek Jo'raqulov",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Opacity(
+                                  opacity: 0.5,
+                                  child: Text(
+                                    "+998 946110464",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const RotatedBox(
+                              quarterTurns: 3,
+                              child: Icon(
+                                Icons.arrow_back_ios_new_rounded,
+                                color: Colors.white,
+                              ),
+                            ),
                           ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
